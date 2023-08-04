b7d30c2132476613afc1ecf67a7878a3c2921223
chalie00 <formathead79@gmail.com>
2023-08-04 16:15:52 +0900

    1) divide Move Tab and Smart Web control 2) Port Text Control 3) CH4 Full Screen 4) CH4 Information Display 5) Full Screen Capture

--- @UserFile/Login.ts
+++ @UserFile/Login.ts
@@ -1,13 +1,65 @@
 
 import { test, expect, Page } from '@playwright/test';
 
-export async function login(page: Page, id: string, pw: string) {
+export async function login(page: Page, url: string, id: string, pw: string) {
+    //open Url
+    //await page.goto(url);
+    await page.goto(url, { waitUntil: 'networkidle' });
 
     //Typing ID
     await page.locator('input[name = login_id]').fill(id);
-  
+
     //Typing PW
     await page.locator('input[name = login_pwd]').fill(pw);
 
 }
 
+export async function timeTable_On_Off(page: Page) {
+    //TimeTable Enable
+    await page.locator('#qtcanvas').click({
+        position: {
+            x: 170,
+            y: 580
+        }
+    });
+
+    //Wait For Smart Web Viewer
+    await page.waitForTimeout(2000);
+
+    //Time Table Disable
+    await page.locator('#qtcanvas').click({
+        position: {
+            x: 153,
+            y: 396
+        }
+    });
+
+}
+
+export async function canvasClickWith_X_Y(page: Page, canvas: string, x: number, y: number) {
+    await page.locator(canvas).click({
+        position: {
+            x: x,
+            y: y
+        }
+    })
+}
+
+export async function canvas_Double_ClickWith_X_Y(page: Page, canvas: string, x: number, y: number) {
+    await page.locator(canvas).dblclick({
+        position: {
+            x: x,
+            y: y
+        }
+    })
+}
+
+export async function keyPressWithCount(page: Page, keyStr: string, count: number) {
+    for (let i = 0; i < count; i+= 1) {
+        await page.keyboard.press(keyStr);    }
+}
+
+export async function changeTextField(page:Page, keyStr: string) {
+    await page.waitForTimeout(1000);
+    await page.keyboard.type(keyStr);
+}
--- tests/NVR Web.spec.ts
+++ tests/NVR Web.spec.ts
@@ -1,27 +1,54 @@
 import { test, expect, Page } from '@playwright/test';
-import { login } from '../@UserFile/Login';
+import { canvasClickWith_X_Y, canvas_Double_ClickWith_X_Y, changeTextField, keyPressWithCount, login, timeTable_On_Off } from '../@UserFile/Login';
 
-const url = 'http://192.168.22.223/cgi-bin/login.cgi';
-const correctID = 'admin';
-const correctPW = '11qqaazz..';
+//Variable of Url, ID
+let url = 'http://192.168.22.223/cgi-bin/login.cgi';
+let correctID = 'admin';
+let correctPW = '11qqaazz..';
 
-const wrongID: string = 'administrator';
-const wrongPW: string = '..zzaaqq11';
+let wrongID: string = 'administrator';
+let wrongPW: string = '..zzaaqq11';
 
+//Element Position in Canvas
+const smartCanvas = '#qtcanvas';
+
+//Smart Web Viewer Canvas Position Information
+const obj_SmartView = {
+    portX: 159,
+    portY: 79,
+    camList_X: 96,
+    camList_Y: 621,
+    screenMode_X: 171,
+    screenMode_Y: 78,
+    ptz_X: 222,
+    ptz_Y: 619
+}
+
+//CH4 Canvas Position Information
+const obj_Ch4 = {
+    area_DCLK_X: 922,
+    area_DCLK_Y: 413,
+    div4_Info_X: 1145,
+    div4_Info_Y: 354,
+    div1_Info_X: 1167,
+    div1_Info_Y: 45
+}
+
+
+/* =============================== Login Sequence  =================================== 
+    Sequence 1
+    Connect URL -> Correct Login -> ScreenShot -> Logout -> Logout
+    Condition: Use '로그인' button 
+
+    Sequence2
+    Connect URL -> Wrong Login -> Correct Login -> Logout
+    Condition: Use Enter Key
+===================================================================================== */
 test.describe('Login Page', () => {
     test('Login / Logout', async ({ page }) => {
-        /* =============================== Login Sequence 1 =================================== 
-            Connect URL -> Correct Login -> Logout -> Logout
-            Condition: Use '로그인' button 
-           ===================================================================================== */
-        //open Url
-        await page.goto(url);
-
-        //Wait for URL
-        await page.waitForTimeout(2000);
-
+    //Sequence 1
         //Login With Correct User Info
-        login(page, correctID, correctPW);
+        login(page, url, correctID, correctPW);
 
         //Wait for URL
         await page.waitForTimeout(2000);
@@ -29,22 +56,20 @@ test.describe('Login Page', () => {
         //Login Click
         await page.locator('#submit').filter({ hasText: /로그인/ }).click();
 
-        //click the Logout
-        await page.getByRole('button', { name: '로그아웃' }).click();
-
-        /* =============================== Login Sequence 2 =================================== 
-            Connect URL -> Wrong Login -> Correct Login -> Logout
-            Condition: Use Enter Key
-           ===================================================================================== */
-
-        //open Url
-        await page.goto(url);
-
         //Wait for URL
         await page.waitForTimeout(2000);
 
+        //Login Page ScreenShot
+        await page.screenshot({ path: './@Captured/AfterLogin.png', fullPage: true });
+
+
+        //click the Logout
+        await page.getByRole('button', { name: '로그아웃' }).click();
+
+
+    //Sequence 2
         //Login With Wrong User Info
-        login(page, wrongID, wrongPW);
+        login(page, url, wrongID, wrongPW);
 
         //Wait for URL
         await page.waitForTimeout(2000);
@@ -56,7 +81,7 @@ test.describe('Login Page', () => {
         await page.waitForTimeout(3000);
 
         //Login With Correct User Info
-        login(page, correctID, correctPW);
+        login(page, url, correctID, correctPW);
 
         //Wait for URL
         await page.waitForTimeout(2000);
@@ -65,7 +90,8 @@ test.describe('Login Page', () => {
         await page.locator('#login_pwd').press('Enter');
 
         //Wait for URL
-        await page.waitForTimeout(2000);
+        //await page.waitForTimeout(2000);
+        await page.goto(url, { waitUntil: 'networkidle' });
 
         //click the Logout
         await page.getByRole('button', { name: '로그아웃' }).click();
@@ -78,36 +104,25 @@ test.describe('Login Page', () => {
 })//End Of Login Test
 
 
+/* =============================== Move Tab Sequenc =================================== 
+    Connect URL -> Correct Login -> Move to [설정] -> Move to [실시간 영상] -> Expanstion Ch4
+    Condition: Use Enter Key
+===================================================================================== */
 test.describe('Smart Web Viewer', () => {
     test('Move Tab', async ({ page }) => {
 
-        /* =============================== Move Tab Sequenc =================================== 
-        Connect URL -> Correct Login -> Move to [설정] -> Move to [실시간 영상] -> Expanstion Ch4
-        Condition: Use Enter Key
-        ===================================================================================== */
-
-
-        //open Url
-        await page.goto(url);
-
-        //Wait for URL
-        await page.waitForTimeout(2000);
-
-        //Login Page ScreenShot
-        await page.screenshot({ path: '../@UserFile/Screenshot.png', fullPage: true });
-
         //Login With Correct User Info
-        login(page, correctID, correctPW);
+        login(page, url, correctID, correctPW);
 
         //Wait for URL
         await page.waitForTimeout(2000);
-
 
         //Push the Enter Key
         await page.locator('#login_pwd').press('Enter');
 
         //Wait for URL
-        await page.waitForTimeout(10000);
+        // await page.waitForTimeout(10000);
+        await page.goto(url, { waitUntil: 'networkidle' });
 
         //Click the Setting
         await page.getByRole('button', { name: '설정' }).
@@ -119,69 +134,57 @@ test.describe('Smart Web Viewer', () => {
         //click the Real Time Stream
         await page.getByRole('button', { name: '실시간 영상' }).click();
 
-        //Wait For Smart Web Viewer
+    })//End Of The Move Tab
+
+
+/* =============================== Smart Web Control Sequenc =================================== 
+    Connect URL -> Correct Login -> Port Text Delete -> Port Set 81 -> CH4 Full Screen 
+    -> CH4 Information On -> Full Screen Capture
+===================================================================================== */
+    test('Smart Web Viewer Control The Element', async ({ page }) => {
+        //Login With Correct User Info
+        login(page, url, correctID, correctPW);
+
+        //Wait for URL
+        await page.waitForTimeout(2000);
+
+        //Push the Enter Key
+        await page.locator('#login_pwd').press('Enter');
+
+        //Wait for URL
         await page.waitForTimeout(5000);
 
+        //Web Port Typing
+        canvasClickWith_X_Y(page, smartCanvas, obj_SmartView.portX, obj_SmartView.portY);
+
+        //Wait for URL
+        await page.waitForTimeout(1000);
+
+        //Port Text Delete and Typing
+        keyPressWithCount(page, 'Backspace', 2);
+        changeTextField(page,'81');
+
+        //Wait for URL
+        await page.waitForTimeout(1000);
+
         //Ch4 Double Click
-        await page.locator('#qtcanvas').dblclick({
-            button: 'left',
-            position: {
-                x: 922,
-                y: 413
-            }
-        });
+        canvas_Double_ClickWith_X_Y(page, smartCanvas, obj_Ch4.div4_Info_X ,obj_Ch4.div4_Info_Y);
 
-        //Wait For Smart Web Viewer
-        await page.waitForTimeout(2000);
+        //Wait for URL
+        await page.waitForTimeout(1000);
 
-        //Mouse Hover the Ch4 for clicking UI[i]
-        await page.locator('#qtcanvas').hover({
-            position: {
-                x: 1167,
-                y: 45
-            }
-        });
+        //Click CH4 UI [i]
+        canvasClickWith_X_Y(page, smartCanvas, obj_Ch4.div1_Info_X, obj_Ch4.div1_Info_Y);
 
-        //Click UI [i]
-        // await page.locator('#qtcanvas').click({
-        //     button: 'left',
-        //     clickCount: 2,
-        //     delay: 2000,
-        //     position: {
-        //         x: 1167,
-        //         y: 45
-        //     }
-        // });
+        //Wait for URL
+        await page.waitForTimeout(1000);
 
-        await page.locator('#qtcanvas').click({
-            position: {
-                x: 170,
-                y: 580
-            }
-        });
-
-        //Wait For Smart Web Viewer
-        await page.waitForTimeout(2000);
-
-        await page.locator('#qtcanvas').click({
-            position: {
-                x: 153,
-                y: 396
-            }
-        });
+        //CH4 FullScreen and Information On ScreenShot
+        await page.screenshot({ path: './@Captured/CH4_Full_Info.png', fullPage: true });
 
 
+        //Keep Browser
+        //await new Promise(() => { });
 
-        // //Wait For Smart Web Viewer
-        // await page.waitForTimeout(2000);
-
-        // //Click [i]PopUp [X]
-        // await page.locator('#qtcanvas').click({
-        //     position: {
-        //         x: 549,
-        //         y: 444
-        //     }
-        // });
-
-    })
+    })//End Of The Smart Web Viewer Control The Element
 })
